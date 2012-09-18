14d8bc3bcc36062e3155d4f4a514060a855bc2d6
Andrew Ferguson <adf@cs.brown.edu>
2012-09-18 09:11:07 -0400

    Tweaks to labels

--- python/history_plot.py
+++ python/history_plot.py
@@ -146,25 +146,25 @@ savefig(open('EV_history-unlabeled.png', 'w'), dpi=62.5, facecolor='#fcfcf4',
 
 # July 12
 day=194
-annotate("Romney tax\nattacks begin", xy=(day, medianDem[day-campaign_start]), xytext=(float(day) + 0.01,
-	medianDem[day-campaign_start]+42), textcoords='data', arrowprops=dict(facecolor='darkblue',
+annotate("Bain", xy=(day, medianDem[day-campaign_start]-2), xytext=(float(day) + 0.01,
+	medianDem[day-campaign_start]-42), textcoords='data', arrowprops=dict(facecolor='darkblue',
 	edgecolor='darkblue', shrink=0.075, width=0.5, headwidth=4),
 	horizontalalignment='center', verticalalignment='top', fontsize=12)
 # August 11
 day=224
-annotate("Ryan\nas VP", xy=(day, medianDem[day-campaign_start]), xytext=(float(day) + 0.01,
-	medianDem[day-campaign_start]-42), textcoords='data', arrowprops=dict(facecolor='darkblue',
+annotate("Ryan\nas VP", xy=(day, medianDem[day-campaign_start]+2), xytext=(float(day) + 0.01,
+	medianDem[day-campaign_start]+42), textcoords='data', arrowprops=dict(facecolor='darkblue',
 	edgecolor='darkblue', shrink=0.075, width=0.5, headwidth=4),
 	horizontalalignment='center', verticalalignment='top', fontsize=12)
 # August 30
 day=243
-annotate("RNC", xy=(day, medianDem[day-campaign_start]), xytext=(float(day) + 0.01,
+annotate("RNC", xy=(day, medianDem[day-campaign_start]+2), xytext=(float(day) + 0.01,
 	medianDem[day-campaign_start]+42), textcoords='data', arrowprops=dict(facecolor='darkblue',
 	edgecolor='darkblue', shrink=0.075, width=0.5, headwidth=4),
 	horizontalalignment='center', verticalalignment='top', fontsize=12)
 # Sept 6
 day=250
-annotate("DNC", xy=(day, medianDem[day-campaign_start]), xytext=(float(day) + 0.01,
+annotate("DNC", xy=(day, medianDem[day-campaign_start]-2), xytext=(float(day) + 0.01,
 	medianDem[day-campaign_start]-42), textcoords='data', arrowprops=dict(facecolor='darkblue',
 	edgecolor='darkblue', shrink=0.075, width=0.5, headwidth=4),
 	horizontalalignment='center', verticalalignment='top', fontsize=12)
