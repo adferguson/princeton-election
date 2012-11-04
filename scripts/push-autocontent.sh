f564b76a9ce9c75adbca4659d8b1537cee7cf13b
Andrew Ferguson <adf@cs.brown.edu>
2012-11-04 14:41:54 -0500

    add quanto to auto-pushing

--- scripts/push-autocontent.sh
+++ scripts/push-autocontent.sh
@@ -5,12 +5,21 @@ SRC_PATH="/home/adf/pec/princeton-election"
 
 cd $SRC_PATH/autographics
 scp *.png swang@synapse.princeton.edu:/var/www/html/autographics/
+scp *.png election@quanto.cs.brown.edu:/home/election/autographics/
 
 cd $SRC_PATH/autotext
 scp *.html swang@synapse.princeton.edu:/var/www/html/autotext/
+cd ../python
+./current_ev-2.py
+mv current_ev.html ../autotext
+cd ../autotext
+scp *.html election@quanto.cs.brown.edu:/home/election/autotext/
 
 cd $SRC_PATH/matlab
 scp *.csv swang@synapse.princeton.edu:/var/www/html/code/matlab/
+scp *.csv election@quanto.cs.brown.edu:/home/election/matlab/
 
 cd $SRC_PATH/python/processed
 scp *.csv swang@synapse.princeton.edu:/var/www/html/code/python/processed/
+scp *.csv election@quanto.cs.brown.edu:/home/election/python/processed/
+
